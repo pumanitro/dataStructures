fe36c723ea7565b44323fe5c621ffe193ac4bda4
pumanitro <pumanitro@gmail.com>
2016-10-30 10:41:33 +0100

    Project initialization and created tree element structure

---
+++ Binary tree/Binary tree/Binary tree.cpp
@@ -0,0 +1,19 @@
+// Binary tree.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+struct treeElement {
+	int key;
+	treeElement *left;
+	treeElement *right;
+	char word[100];
+};
+
+int main()
+{
+	//test
+
+    return 0;
+}
+
