44976e71a1b29de483c37db89417c6678a381eff
pumanitro <pumanitro@gmail.com>
2016-10-30 12:13:17 +0100

    Added clock and file support.

--- Binary tree/Binary tree/Binary tree.cpp
+++ Binary tree/Binary tree/Binary tree.cpp
@@ -1,7 +1,11 @@
-// Binary tree.cpp : Defines the entry point for the console application.
+﻿// Binary tree.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
+#include <time.h>       /* time */
+#include <iostream>
+
+using namespace std;
 
 struct treeElement {
 	int key;
@@ -10,9 +14,47 @@ struct treeElement {
 	char word[100];
 };
 
+
+
 int main()
 {
-	//test2
+	/* initialize random seed: */
+	srand(time(NULL));
+
+	//Time start:
+	clock_t begin, end;
+	double time_spent;
+	begin = clock();
+
+	//File read:
+	int X, k1, k2, k3, k4;
+	FILE* fp;
+	errno_t err;
+
+	err = fopen_s(&fp, "inputData.txt", "r");
+	if (err != 0)
+		return -1;
+	fscanf_s(fp, "%d %d %d %d %d", &X, &k1, &k2, &k3, &k4);
+	fclose(fp);
+
+	printf("I read from file %d %d %d %d %d\n\n", X, k1, k2, k3, k4);
+
+	//CONTENT :
+
+
+	
+
+	//Time stop:
+	end = clock();
+	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+	//Show time:
+	printf("\nTime of process : %f\n", time_spent);
+
+	//Wait for sign: 
+	cin.sync();
+	cin.get();
+
     return 0;
 }
 
